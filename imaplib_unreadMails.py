f9fd16d83ef587f5e9b484ad1690452da2f6dfc5
Homwer <homwer@web.de>
2017-08-01 15:11:27 +0200

    added uer input for mail and server data

--- imaplib_unreadMails.py
+++ imaplib_unreadMails.py
@@ -1,8 +1,20 @@
-### Abfrage; wie viele ungelesene Mails liegen im Postfach
+### Sends a request to the imap server for unread mails.
 
 import imaplib
-mail = imaplib.IMAP4_SSL('alphard.uberspace.de')
-mail.login('philipp@codingworld.io', 'PASSWORT')
+
+# Ask the user for their information
+server = raw_input("Please enter your Imap Server: ")
+email = raw_input("Please enter your Mailadress: ")
+password = raw_input("Please enter your password: ")
+
+# Enter your data here, and uncomment, unsafe but handy solution !
+#server = "SERVER"
+#email = "Mailadress"
+#password = "PASSWORD"
+
+
+mail = imaplib.IMAP4_SSL(server)
+mail.login(email, password)
 mail.list()
 mail.select("inbox")
 data = mail.search(None, 'UNSEEN')
